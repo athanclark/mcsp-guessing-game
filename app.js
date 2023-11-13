a5d3b09d22f8d0b986e866fe0713ef05d5837b48
Athan Clark <athan.clark@gmail.com>
2023-11-13 12:55:25 -0500

    added some semicolons

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 function mainFunction(answer){
     // Accepts a number representing the number to try to guess
     // Displays an alert telling them if they were correct, or if they should guess higher or lower
-    console.log("answer", answer)
-    var guess = window.prompt('Guess a number between 1 and 100')
-    console.log("guess", guess)
+    console.log("answer", answer);
+    var guess = window.prompt('Guess a number between 1 and 100');
+    console.log("guess", guess);
     if (answer > guess){
-        alert("Your guess is too low!")
+        alert("Your guess is too low!");
     } else if (answer < guess){
-        alert("Your guess is too high!")
+        alert("Your guess is too high!");
     } else if (answer == guess){
-        alert("Correct! Nice guess")
+        alert("Correct! Nice guess");
     }
 }
 
 function generateRandomNum(){
-  // Generates a random integer between 1 and 100
-  return Math.floor(Math.random() * (101 - 1)) + 1
+    // Generates a random integer between 1 and 100
+    return Math.floor(Math.random() * (101 - 1)) + 1;
 }
 
 mainFunction(generateRandomNum());
