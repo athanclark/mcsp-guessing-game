70c0c7ca551b0cdfc80d28c6832ce6caaa4e0739
Jack Dutton <duttonjack@Jacks-MacBook-Pro.local>
2023-11-13 14:46:49 -0500

    Feature 7 implemented

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ function generateRandomNum(){
     // Generates a random integer between 1 and 100
     return Math.floor(Math.random() * (101 - 1)) + 1;
 }
+// Keys are names, values are number of guesses
+var scores = {};
 
-var name = prompt('Enter your name');
-
-function initializeGame() {
+var initializeGame = function initializeGame() {
+    var name = prompt('Enter your name');
     var answer = generateRandomNum();
     var guesses = [];
 
@@ -23,7 +24,17 @@ function initializeGame() {
             alert(`Sorry ${name}. Your guess is too high!`);
             return false;
         } else if (answer === guess){
-            alert(`That's Correct ${name}! Nice guess - guesses: ${guesses.slice(0, guesses.length - 1)}`);
+            if (scores[name] === undefined){
+                alert(`That's Correct ${name}! Nice guess - guesses: ${guesses.slice(0, guesses.length - 1)}`);
+                scores[name] = guesses.length
+            } else if (scores[name]=== guesses.length){
+                alert(`That's Correct ${name}! Nice guess - guesses: ${guesses.slice(0, guesses.length - 1)} You had the same number of guesses as your best attempt!`);
+            } else if (scores[name] < guesses.length){
+                alert(`That's Correct ${name}! Nice guess - guesses: ${guesses.slice(0, guesses.length - 1)} Your best score had ${guesses.length - scores[name]} fewer attempts.`);
+            } else if (scores[name] > guesses.length){
+                alert(`That's Correct ${name}! Nice guess - guesses: ${guesses.slice(0, guesses.length - 1)} This score had ${scores[name] - guesses.length} fewer attempts than your old best!.`);
+                scores[name] = guesses.length
+            }
             return true;
         }
     };
