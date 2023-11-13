ae70b4c478ebfe9772a8d84c2ec6c8ad0fca9f32
Jack Dutton <duttonjack@Jacks-MacBook-Pro.local>
2023-11-13 12:45:22 -0500

    Feature 1 of guessing game

---
+++ app.js
@@ -0,0 +1,22 @@
+function mainFunction(answer){
+    // Accepts a number representing the number to try to guess
+    // Displays an alert telling them if they were correct, or if they should guess higher or lower
+    console.log("answer", answer)
+    var guess = window.prompt('Guess a number between 1 and 100')
+    console.log("guess", guess)
+    if (answer > guess){
+        alert("Your guess is too low!")
+    } else if (answer < guess){
+        alert("Your guess is too high!")
+    } else if (answer == guess){
+        alert("Correct! Nice guess")
+    }
+}
+
+function generateRandomNum(){
+  // Generates a random integer between 1 and 100
+  return Math.floor(Math.random() * (101 - 1)) + 1
+}
+
+mainFunction(generateRandomNum());
+
