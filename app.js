e8e0a0b1e7ff48571e90277c8f826cb5703f03ff
Athan Clark <athan.clark@gmail.com>
2023-11-13 14:27:06 -0500

    encapsulated main function, allow for replaying

--- app.js
+++ app.js
@@ -3,30 +3,37 @@ function generateRandomNum(){
     return Math.floor(Math.random() * (101 - 1)) + 1;
 }
 
-var answer = generateRandomNum();
-var guesses = [];
+var name = prompt('Enter your name');
 
-var mainFunction = function mainFunction(){
-    // Accepts a number representing the number to try to guess
-    // Displays an alert telling them if they were correct, or if they should guess higher or lower
-    console.log("answer", answer);
-    var guess = Number(prompt('Guess a number between 1 and 100'));
-    guesses.push(guess);
-    console.log("guess", guess);
-    if (answer > guess){
-        alert("Your guess is too low!");
-        return false;
-    } else if (answer < guess){
-        alert("Your guess is too high!");
-        return false;
-    } else if (answer === guess){
-        alert(`Correct! Nice guess - guesses: ${guesses.slice(0, guesses.length - 1)}`);
-        return true;
-    }
-};
+function initializeGame() {
+    var answer = generateRandomNum();
+    var guesses = [];
 
-var outcome;
+    var mainFunction = function mainFunction(){
+        // Accepts a number representing the number to try to guess
+        // Displays an alert telling them if they were correct, or if they should guess higher or lower
+        console.log("answer", answer);
+        var guess = Number(prompt('Guess a number between 1 and 100'));
+        guesses.push(guess);
+        console.log("guess", guess);
+        if (answer > guess){
+            alert(`Sorry ${name}, Your guess is too low!`);
+            return false;
+        } else if (answer < guess){
+            alert(`Sorry ${name}. Your guess is too high!`);
+            return false;
+        } else if (answer === guess){
+            alert(`That's Correct ${name}! Nice guess - guesses: ${guesses.slice(0, guesses.length - 1)}`);
+            return true;
+        }
+    };
+
+    do {
+        var outcome = mainFunction();
+    } while (!outcome)
+}
+
 do {
-    outcome = mainFunction();
-} while (!outcome)
-
+    initializeGame();
+    var again = prompt('Would you like to play again? (Y/N)');
+} while (again === 'Y')
